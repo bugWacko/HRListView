603889547b1de32f9eeb1a7878206202c9bebe38
陈晨昕 <bug_wacko@163.com>
2017-06-14 10:25:29 +0800

    Initial Commit

---
+++ HRListView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HRListView
+//
+//  Created by 陈晨昕 on 2017/6/14.
+//  Copyright © 2017年 bugWacko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
